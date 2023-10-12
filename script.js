b8dde5d06c52383517cce30a3b5be2f5f5d0bb93
Ilia F <Ilia>
2023-10-12 18:51:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm() {
+
+  isValid = form.checkValidity();
+
+  if (!isValid) {
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    return;
+  }
+}
